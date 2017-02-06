38e97c2d01eaf5ad0caea199df2f4144d288d673
Michael Florez <mike_florez@ymail.com>
2017-02-06 01:30:40 -0500

    done

--- swift-shopping-cart/Cart.swift
+++ swift-shopping-cart/Cart.swift
@@ -8,5 +8,56 @@
 
 class Cart {
     
+    var items: [Item] = []
+    
+    func totalPriceInCents() -> Int {
+        var total = 0
+        
+        for price in items {
+            total += price.priceInCents
+        }
+        return total
+    }
+    
+    func add(item: Item) {
+        items.append(item)
+    }
+    
+    func remove(item: Item) {
+        if let removeAtIndex = items.index(of: item) {
+            items.remove(at: removeAtIndex)
+        }
+    }
+    
+    func items(withName name: String) -> [Item] {
+        var matchingItems: [Item] = []
+        for item in items {
+            if item.name == name {
+                matchingItems.append(item)
+            }
+        }
+        return matchingItems
+    }
+    
+    func items(withMinPrice price: Int) -> [Item] {
+        var itemsGreater: [Item] = []
+        for item in items {
+            if item.priceInCents >= price {
+                itemsGreater.append(item)
+            }
+        }
+        return itemsGreater
+    }
+    
+    func items(withMaxPrice price: Int) -> [Item] {
+        var itemsLess: [Item] = []
+        for item in items {
+            if item.priceInCents <= price {
+                itemsLess.append(item)
+            }
+        }
+        return itemsLess
+    }
+    
     
 }
